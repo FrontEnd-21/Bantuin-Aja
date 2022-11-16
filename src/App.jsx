b1258a35b8f3abe61bec3863ecd8aa3d48c54e2b
Sabrinaaafz <fauziasabrina0@gmail.com>
2022-11-16 14:05:07 +0700

    install bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./Pages/Home";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";

bd3f45fcb6bd0054246689c293d396436f1c0c1a
Sabrinaaafz <fauziasabrina0@gmail.com>
2022-11-16 13:37:18 +0700

    merubah tata letak code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
 import "./App.css";
 import Home from "./Pages/Home";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Home />
+      <div className='App'>
+        <div className='nav'>
+          <Navbar />
+        </div>
+        <div className='home'>
+          <Home />
+        </div>
+        <div className='footer'>
+          <Footer />
+        </div>
+      </div>
     </>
   );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,25 +1,15 @@
 import React from "react";
 import AboutCard from "../Components/AboutCard/AboutCard";
 // import DataCompany from "./Components/DataCompany/DataCompany";
-import Footer from "../Components/Footer";
-import Navbar from "../Components/Navbar";
 
 const Home = () => {
   return (
     <>
-      <div className='App'>
-        <div className='nav'>
-          <Navbar />
-        </div>
-        {/* <div className="data">
+      {/* <div className="data">
         <DataCompany />
       </div> */}
-        <div className='at'>
-          <AboutCard />
-        </div>
-        <div className='footer'>
-          <Footer />
-        </div>
+      <div className='at'>
+        <AboutCard />
       </div>
     </>
   );
